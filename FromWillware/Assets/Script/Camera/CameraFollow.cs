fb3854a03f8f80078ddafc72bde5ead3fc3adf67
zswzhijing <3103949571@qq.com>
2026-04-03 21:09:55 +0800

    鼠标隐藏，镜头调整

--- FromWillware/Assets/Script/Camera/CameraFollow.cs
+++ FromWillware/Assets/Script/Camera/CameraFollow.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform PlayerTransform;
-    public float MouseSensitivity;
-    public float Distance;
+    public float MouseSensitivity= 200f;
+    public float Distance= 3.5f;
     public float VerticalOffset = 1.5f;
     public float LookAtOffset = 1.5f;
     public float xRotation = 30f;
     public float yRotation = 0f;
     public float MinDistance = 0.6f;
     public LayerMask ObstacleLayerMask = ~0;
+
+    void Start()
+    {
+        // 隐藏鼠标并锁定光标
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
@@ -28,7 +35,6 @@ public class CameraFollow : MonoBehaviour
         Vector3 direction = rotation * new Vector3(0, 0, -Distance);
 
         Vector3 playerPosition = PlayerTransform.position + new Vector3(0, VerticalOffset, 0);
-        Vector3 targetPosition = playerPosition + direction;
         
         // 检测镜头与玩家之间是否有遮挡
         float adjustedDistance = Distance;
